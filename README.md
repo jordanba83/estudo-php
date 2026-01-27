8ca01d29de969c724a9be56dec84ed2cc34be302
Jordan Gomes <jordanba83@gmail.com>
2026-01-27 03:36:07 +0000

    Treinando controle de versao e estudando php

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # estudo-php
 Estudo sobre php
+Aqui incio minha caminhada!
+comecei com formularios!
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste</title>
+</head>
+<body>
+    <header><h1><strong>Teste de codigo</strong></h1></header>
+    <main>
+        <form action="teste.php" method="post">
+            <label>Nome</label><br>
+            <input type="text" name="nome"><br>
+            <input type="submit" value="ENVIAR">
+
+        </form>
+
+    </main>
+</body>
+</html>
