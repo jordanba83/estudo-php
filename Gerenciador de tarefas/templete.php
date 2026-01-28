23f2927353e07ef15bfb59ea9aaa9dd75d974ec7
Jordan Gomes <jordanba83@gmail.com>
2026-01-28 01:36:08 +0000

    Testando um gerenciador de tarefas

--- Gerenciador de tarefas/templete.php
+++ Gerenciador de tarefas/templete.php
@@ -1,2 +1,32 @@
 <!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gerenciador de Tarefas</title>
+</head>
+<body>
+    <h1>Gerefenciador de Tarefas</h1>
+    <form>
+        <fieldset>
+            <legend>
+                <label>
+                    Tarefa:
+                    <input type="text" name="nome" />
+                    
+                </label>
+                <input type="submit" value="Cadastrar" />
+            </legend>
+        </fieldset>
+    </form>
+    <table>
+        <tr>
+            <th>Tarefas</th>
+        </tr>
+        <?php foreach ($lista_tarefas as $tarefas) : ?>
+            <tr>
+                <?php endforeach; ?>
+            </tr>
+    </table>
+</body>
 </html>
